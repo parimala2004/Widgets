c8a5b37fc2e21a51ea78dafb602359fba1701819
parimala2004 <145036239+parimala2004@users.noreply.github.com>
2025-05-13 20:42:21 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -87,6 +87,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
               ),
             ),
               MainPage(),
+             Screen(),
         ]
           ),
         );
